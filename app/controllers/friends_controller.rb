863a590fb836d26456f473f28118dce9cb60d359
AJ Villalobos <hi@ajvillalobos.com>
2021-11-19 00:17:20 +0800

    feat: finalize styling and links for friends list

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,6 +1,5 @@
 class FriendsController < ApplicationController
   def show
-    user = User.find_by(username: params[:username])
-    @friends = user.friends
+    @user = User.find_by(username: params[:username])
   end
 end
